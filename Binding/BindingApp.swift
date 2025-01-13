08f7a12230f58b325f0aa1d94409b36e1b51328a
khurram-iqbal-tech <khurram.ms1@gmail.com>
2025-01-13 20:11:44 +0500

    Initial Commit

---
+++ Binding/BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingApp.swift
+//  Binding
+//
+//  Created by MacBook Pro on 13/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BindingTests/BindingTests.swift
@@ -0,0 +1,17 @@
+//
+//  BindingTests.swift
+//  BindingTests
+//
+//  Created by MacBook Pro on 13/01/2025.
+//
+
+import Testing
+@testable import Binding
+
+struct BindingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
